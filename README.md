71f3cf3b5341f08462ab5d162ccb38af4c0d53a2
Priyansh Garg <priyanshgarg30@gmail.com>
2024-07-01 00:19:18 +0530

    Refactor and modify usage.

--- README.md
+++ README.md
@@ -2,19 +2,20 @@
 
 ## Usage
 
-### Create a square matrix
+### 1. Create a square matrix
 
-**Usage:** `cargo run --bin create [<matrix-size> <out-file>]`
+**Usage:** `cargo run --bin create [<out-file> <matrix-size>]`
 
 ```sh
-# Output a square matrix in 'matrix.txt' file.
-# Program will ask for matrix size.
+# output a square matrix to 'matrix.txt' file.
+# program will ask for matrix size.
 cargo run --bin create
 
-# output a square matrix of size 6 in 'matrix.txt' file.
-cargo run --bin create 6
+# output a square matrix to 'matrix_2.txt' file.
+# program will ask for matrix size.
+cargo run --bin create matrix_2.txt
 
-# output a square matrix of size 10 in 'new_matrix.txt' file
-cargo run --bin create 10 new_matrix.txt
+# output a square matrix of size 10 to 'new_matrix.txt' file
+cargo run --bin create new_matrix.txt 10
 ```
 
--- src/bin/create.rs
+++ src/bin/create.rs
@@ -1,36 +1,34 @@
 use rand::Rng;
 use std::env;
-use std::fs::File;
+use std::fs;
 use std::io::{self, Write};
 
 fn main() {
-    let mut size = String::new();
-
     let mut args_iter = env::args();
     // first item not useful to us
     args_iter.next();
 
-    // define size of matrix
-    if let Some(value) = args_iter.next() {
-        size = value;
-    } else {
-        print!("Enter matrix size: ");
-        io::stdout().flush().unwrap();
-
-        io::stdin()
-            .read_line(&mut size)
-            .expect("Failed to read input");
-    }
-
-    let size: u32 = size.trim().parse().expect("Matrix size should be a number.");
-
-    // define file in which matrix should be saved
     let file_name = if let Some(value) = args_iter.next() {
         value
     } else {
         "matrix.txt".to_string()
     };
-    let mut file = File::create(&file_name).expect("Failed to create file");
+
+    let size: String = if let Some(value) = args_iter.next() {
+        value
+    } else {
+        let mut input = String::new();
+
+        print!("Enter matrix size: ");
+        io::stdout().flush().unwrap();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+
+        input
+    };
+    let size: u32 = size.trim().parse().expect("Matrix size should be a number.");
 
     // random number generator (for matrix elements)
     let mut thread_rng = rand::thread_rng();
@@ -45,7 +43,9 @@ fn main() {
         final_matrix.push('\n');
     });
 
-    file.write(final_matrix.as_bytes()).expect("Failed to write matrix into file");
+    if let Err(error) = fs::write(&file_name, final_matrix.as_bytes()) {
+        panic!("Error while creating or writing to file: {error}");
+    }
 
     println!("Created a matrix of size {size} in '{file_name}'!");
 }
