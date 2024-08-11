4602a4c557fefe0126f0266ff88caac9b936ca0a
Priyansh Garg <priyanshgarg30@gmail.com>
2024-08-11 16:18:23 +0530

    Simplify Display trait impl for Matrix.

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,7 @@ pub struct Matrix {
 
 impl fmt::Display for Matrix {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut matrix_repr = String::new();
-
-        let first_row = (0..self.col).map(|_| "  ".to_string()).collect::<Vec<String>>().join(" ");
-
-        matrix_repr.push_str(format!("\n--{first_row}--\n").as_str());
+        let mut matrix: Vec<String> = Vec::new();
         for i in 0..self.row {
             let row_start = i*self.col;
             let row_end = (i+1)*self.col;
@@ -23,13 +19,9 @@ impl fmt::Display for Matrix {
                 .collect::<Vec<String>>()
                 .join(" ");
 
-            matrix_repr.push_str("| ");
-            matrix_repr.push_str(&matrix_row);
-            matrix_repr.push_str(" |\n");
+            matrix.push(matrix_row);
         }
-        matrix_repr.push_str(format!("--{first_row}--\n").as_str());
-
-        write!(f, "{}", matrix_repr)
+        write!(f, "{}", matrix.join("\n"))
     }
 }
 
